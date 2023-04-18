a188f85f78fe92f20116d026f12b47bca868365a
Gustavo Silva <ukoruja@gmail.com>
2023-04-18 02:18:42 -0300

    commit inicial

---
+++ assents/js/questoes.js
@@ -0,0 +1,114 @@
+let questoes = [
+    {
+        questao: 'Quando foi a última vez que o Brasil ganhou a Copa do Mundo?',
+        opcoes:[
+            1998,
+            2000,
+            2002,
+            2004,
+            2006
+        ],
+        resposta: 2
+    },
+    
+    {
+        questao: 'Quem foi o técnico que conquistou a primeira Copa do mundo para o Brasil?',
+        opcoes:[
+            'Vicente Feola, o bonachão camarada',
+            'Aymoré Moreira foi o técnico do Bi no Chile',
+            'Paulo Machado, o Marechal da Vitória',
+            'Zagallo, um ponta esforçado e vencedor',
+            'Parreira'
+        ],
+        resposta: 2
+    },
+    {
+        questao: 'Quantas copas do mundo foram conquistadas pela Seleção Brasileira Feminina?',
+        opcoes:[
+            0,
+            2,
+            4,
+            5,
+            7
+        ],
+        resposta: 4
+    },
+    {
+        questao: 'Em 2005, qual brasileiro ganhou a Bola de Ouro?',
+        opcoes:[
+            'Ronaldinho Gaucho',
+            'Neymar',
+            'Ronaldo Fenomeno',
+            'Kaka',
+            'Pelé'
+        ],
+        resposta: 0
+        
+    },
+    {
+        questao: 'Quem foi o último brasileiro a ganhar a Bola de Ouro?',
+        opcoes:[
+            'Ronaldinho Gaucho',
+            'Neymar',
+            'Ronaldo Fenomeno',
+            'Kaka',
+            'Romário'
+        ],
+        resposta: 3
+    },
+    {
+        questao: 'A final da copa de 1970 foi:',
+        opcoes:[
+            'Brasil x Holanda',
+            'Brasil x França',
+            'Holanda x Inglaterra',
+            'Argentina x França',
+            'Brasil x Itália'
+        ],
+        resposta: 4
+    },
+    {
+        questao: 'Dos times de futebol a seguir, qual é o único que nunca foi rebaixado?',
+        opcoes:[
+            'Flamengo',
+            'Botafogo',
+            'Vasco',
+            'Fluminense',
+            'Palmeiras'
+        ],
+        resposta: 0
+    },
+    {
+        questao: 'Time que ganhou mais vezes o Campeonato Brasileiro de futebol?',
+        opcoes:[
+            'Flamengo',
+            'Cruzeiro',
+            'Corinthians',
+            'Atlético Mineiro',
+            'Palmeiras'
+        ],
+        resposta: 4
+    },
+    {
+        questao: 'Quem é o maior jogador de futebol da história do Brasil?',
+        opcoes:[
+            'Garrincha',
+            'Zico',
+            'Ronaldo Fenômeno',
+            'Neymar',
+            'Pelé'
+        ],
+        resposta: 4
+    },
+    {
+        questao: 'Em 2010, qual time foi campeão do campeonato carioca?',
+        opcoes: [
+            'Flamengo',
+            'Fluminense',
+            'Vasco',
+            'Botafogo',
+            'Nova Iguaçu'
+        ],
+        resposta: 3
+    },
+];
